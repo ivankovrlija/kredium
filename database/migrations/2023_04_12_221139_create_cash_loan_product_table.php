bf22351d9fcade34fb48bd00f4ae9db9535abe6f
Ivan Kovrlija <kovrlijaivan@gmail.com>
2023-04-13 00:36:54 +0200

    add models and migrations, change user factory, change read me file to explain how to use factory

--- database/migrations/2023_04_12_221139_create_cash_loan_product_table.php
+++ database/migrations/2023_04_12_221139_create_cash_loan_product_table.php
@@ -13,14 +13,17 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cash_loan_product', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('client_id')
+            ->constrained('clients')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->foreignId('user_id')
+            ->constrained('users')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            $table->integer('loan_amount');
             $table->timestamps();
         });
     }
@@ -32,6 +35,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cash_loan_product');
     }
 };
